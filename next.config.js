11729c41404bb522b11dbec72221afa718ae755d
Jose Guilherme Santos Campo <zecampos2015@yahoo.com.br>
2020-07-15 17:42:53 -0300

    add deploy gh pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   exportTrailingSlash: true,
+  assetPrefix: !debug ? "/josecampos/" : "",
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    // Important: return the modified config
+    return config;
+  },
 };
